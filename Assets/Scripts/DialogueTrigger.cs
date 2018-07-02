7e19b800c5c497466743a5ec63afdc9abac9bf71
funkYzWo <angelkozlev@gmail.com>
2018-07-02 10:27:55 +0100

    alpha 0.0.1

---
+++ Assets/Scripts/DialogueTrigger.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour
+{
+
+    public Dialogue dialogue;
+    public bool active = true;
+   
+    void OnTriggerEnter2D(Collider2D other)
+    {
+
+        if (other.tag == "Player")
+        {
+
+            DialogueManager.instance.StartDialogue(dialogue);
+            GameObject.Destroy(gameObject);
+        }
+
+    }
+
+    public void TriggerDialogue()
+    {
+
+        DialogueManager.instance.StartDialogue(dialogue);
+
+    }
+}
